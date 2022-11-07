578b1988d261aec3fa9012cf790de84710e568f2
Ryan Villanueva <ryanueva@gmail.com>
2022-11-07 16:06:00 -0500

    added footer to html, started forming questions in script.js, started adding to css too

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var questions  [
+  {
+    prompt: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    
+  }
+]
