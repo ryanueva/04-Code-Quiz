43ed651fb1be1de4e0461df7c2b4f74d6a5bc753
Ryan Villanueva <ryanueva@gmail.com>
2022-11-17 14:39:30 -0500

    edited html setup, commented out var questions in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,40 @@
-var questions  [
-  {
-    prompt: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
+// var questions = [
+//     {
+//         prompt: "An array’s length can be evaluated with the what property?\n (a).length\n (b).log\n (c)the console\n (d).loop",
+//         answer: "a"
+//       },
     
-  }
-]
+//       {
+//         prompt: "Within a loop, the 'break' keyword may be used to do what?\n (a)break your competitors code\n (b)exit the loop immediately\n (c)repeat the loop\n (d)indicate a stopping condition",
+//         answer: "b"
+//       },
+    
+//       {
+//         prompt: "Properties in a JavaScript oject are often refferred to as what?\n (a)dot walking\n (b)key-value pairs\n (c)nested properties\n (d)undefined",
+//         answer: "b"
+//       },
+    
+//       {
+//         prompt: "Which array method inserts an element at the end of the array?\n (a).pop()\n (b).push()\n (c).length\n (d).join()",
+//         answer: "b"
+//       },
+    
+//       {
+//         prompt: "What is a callback function?\n (a)a function that accepts an array as an argument\n (b)I function that performs an HTTP request\n (c)a data type similar to a string or a boolean\n (d)a function that is passed into another function as an argument",
+//         answer: "a"
+//       }
+// ];
+
+// var score = 0;
+
+// for (var i=0; i < questions.length; i++) {
+//     var response = window.prompt(questions[i].prompt);
+//     if(response == questions[i].answer){
+//         score++;
+//         alert("Correct");
+//     }else {
+//         alert("Wrong!");
+//     }
+// }
+// alert("you got " + socre + "/" + questions.length);\
+
